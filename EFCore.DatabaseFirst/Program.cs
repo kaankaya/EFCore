63f5037f99e9a3979feee65af9648aafd0c716ec
Kaan Kaya <kaankaya.0707@gmail.com>
2024-12-14 01:26:56 +0300

    Add project files.

---
+++ EFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+DbContextInitializer.Build();
+//nesne örneğini alıyoruz
+//using ifade si kullanıyoruz çünkü bu işlem bitince memoryden dispose olsun diye
+//memory de context tutmak mantıklı değil.scope bittiği an memorden uçar
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync(); //git Products tablosu ile haberleş ve ToList ile verilerin hepsini çek
+    products.ForEach(p =>
+    {
+    Console.WriteLine($"{p.Id} - {p.Name} - {p.Price} - {p.Stock}"); //p tablomdaki verilere denk gelir ve sırayla foreach olarak işlemleri listeler
+    });
+}
---
+++ EFCore.DatabaseFirstByScaffold/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.DatabaseFirstByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+using(var _context = new EfcoreDatabaseFirstDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Name} - {p.Price} - {p.Stock}");
+    });
+}
