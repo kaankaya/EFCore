6c401f6f7d7be323377eb4ce00830622a74459f8
Kaan Kaya <kaankaya.0707@gmail.com>
2024-12-15 01:30:27 +0300

    add-migration update-database remove-migration script-migration

---
+++ EFCore.CodeFirst/DAL/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst.DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        //her yapılan bir değişik de yeni migration oluşturmamız gerekiyor senkron olması adına
+        //yeni bir sutun ekledik ve yeni bir migration daha oluşturduk
+        //çalışma prensibine göre önce addmigrate yaptıgımız da snapshot gidiyor bakıyor kıyaslıyor eksik olanı tespit edip ona göre yeni bir migrate oluşturuyor
+        public int Barcode { get; set; }
+    }
+}
